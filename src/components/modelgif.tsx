10d7e14a309f2b8b4dae1881a6af2c3d4ebcaa14
surya-deenu <surya.r.applogiq@gmail.com>
2025-11-17 14:47:57 +0530

    Eagle page fixes

--- src/components/modelgif.tsx
+++ src/components/modelgif.tsx
@@ -11,17 +11,14 @@ export default function ModelGif({ currentProduct, hoveredProduct }: any) {
             <div className="w-full max-h-[80%] flex flex-col items-center justify-center overflow-hidden">
                 <div className="relative w-full max-w-full flex-1 flex items-center justify-center ">
                     {hoveredProduct && currentProduct && (
-                        <div className="max-w-[90%] md:max-w-[80%] max-h-[300px] md:max-h-[500px] m-auto bg-[#0a0e27] rounded-2xl overflow-hidden flex items-center justify-center relative">
-                            <Image
+                        <div className="max-w-[90%] md:max-w-[80%] max-h-[300px] md:max-h-[500px] m-auto bg-[#0a0e27] rounded-2xl overflow-hidden flex items-center justify-center relative mt-4">
+                            <img
                                 src={currentProduct?.gifPath}
                                 alt={`${currentProduct?.title} animation`}
                                 className="object-cover transition-opacity duration-700"
-                                fill
-                                sizes="(max-width: 768px) 90vw, 80vw"
                                 style={{
                                     animation: "imageReveal 0.7s ease-out forwards",
                                 }}
-                                unoptimized
                             />
                         </div>
                     )}
