9b8d487ce9f8ed8681a0720980139a7c7e9c89b5
Naveenkumar6464 <naveenkumar.n@applogiq.org>
2025-12-22 18:27:49 +0530

    Feat : events   secrion responsive dessign

--- src/components/TeamCard/index.tsx
+++ src/components/TeamCard/index.tsx
@@ -20,7 +20,7 @@ const TeamCard: React.FC<TeamCardProps> = ({
       <img
         src={image}
         alt={name}
-        className="absolute inset-0 h-full w-full object-cover transition-transform duration-700 ease-out group-hover:scale-110"
+        className="absolute inset-0 h-full w-full object-cover transition-transform duration-700 ease-out group-hover:scale-104"
       />
 
       {/* BLACK GRADIENT (DEFAULT) */}
@@ -49,9 +49,11 @@ const TeamCard: React.FC<TeamCardProps> = ({
 
       {/* HOVER CONTENT (DESCRIPTION) */}
       <div className="absolute bottom-[-100%] left-0 z-[4] w-full p-6 text-white transition-all duration-500 group-hover:bottom-0">
-        <h3 className="text-lg font-semibold">{name}</h3>
-        <p className="text-sm opacity-90">{role}</p>
-        <p className="mt-3 text-sm leading-relaxed">{description}</p>
+        {/* <h3 className="text-lg font-semibold">{name}</h3> */}
+        {/* <p className="text-sm opacity-90">{role}</p> */}
+        <p className="mt-4 text-[18px] lg:text-[20px] leading-relaxed">
+          {description}
+        </p>
       </div>
     </div>
   );
